ca3134cf33c0cd95382fbdc251c0832d82aec5b9
Pranay Kumar Gupta <techyaar98353@gmail.com>
2026-07-21 11:56:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -2,20 +2,11 @@ class Solution {
   public:
     int kthSmallest(vector<int> &arr, int k) {
         // code here
-        int start=*min_element(arr.begin(),arr.end());
-        int end=*max_element(arr.begin(),arr.end());
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            int countless=0;
-            int countequal=0;
-            for(int i:arr){
-                if(i<mid)countless++;
-                else if(i==mid)countequal++;
-            }
-            if(countless<k && countless+countequal>=k) return mid;
-            else if(countless>=k) end=mid-1;
-            else if(countless<k && countless+countequal<k) start=mid+1;
+        priority_queue<int> q;
+        for(int i:arr){
+            q.push(i);
+            if(q.size()>k) q.pop();
         }
-        return -1;
+        return q.top();
     }
 };
